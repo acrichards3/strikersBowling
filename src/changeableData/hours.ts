793a504b1dd52a5075165301a01de44c2f1d9512
alexrich123 <alexchristopherrichards@hotmail.com>
2022-03-30 15:23:38 -0400

    Hours card

---
+++ src/changeableData/hours.ts
@@ -0,0 +1,39 @@
+const HOURS = [
+  {
+    day: 'Monday',
+    open: '4:00 PM',
+    close: '10:00 PM',
+  },
+  {
+    day: 'Tuesday',
+    open: '12:00 PM',
+    close: '10:00 PM',
+  },
+  {
+    day: 'Wednesday',
+    open: '4:00 PM',
+    close: '10:00 PM',
+  },
+  {
+    day: 'Thursday',
+    open: '5:00 PM',
+    close: '11:00 PM',
+  },
+  {
+    day: 'Friday',
+    open: '5:00 PM',
+    close: '11:00 PM',
+  },
+  {
+    day: 'Saturday',
+    open: '12:00 PM',
+    close: '12:00 AM',
+  },
+  {
+    day: 'Sunday',
+    open: '12:00 PM',
+    close: '7:30 PM',
+  },
+];
+
+export default HOURS;
--- src/components/cards/HoursCard.tsx
+++ src/components/cards/HoursCard.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import HOURS from '../../changeableData/hours';
 import { Card } from '@blueprintjs/core';
+import './hoursCard.css';
 
 export default function HoursCard() {
   return (
     <Card elevation={3}>
-        <h3>Open Hours:</h3>
-        
+      <h3>Open Hours:</h3>
+        {HOURS.map((day) => {
+          return (
+            <h6 className="hour">
+              {day.day}: <div className="weeklyHours">{day.open} - {day.close}</div>
+            </h6>
+          );
+        })}
+      <div className="openBowlingBtn">
+        <Button variant="danger">
+          Open Bowling Schedule
+        </Button>
+      </div>
     </Card>
-  )
+  );
 }
